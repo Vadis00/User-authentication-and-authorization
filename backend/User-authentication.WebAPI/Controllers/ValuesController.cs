675d85fe2481ae3939711464f4c76658c072960b
Vadis00 <v.gomenyuk@std.npu.edu.ua>
2022-10-06 14:30:44 +0300

    added uuth service and controller

---
+++ backend/User-authentication.WebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace User_authentication.WebAPI.Controllers
+{
+    [Route("api/auth")]
+    public class AuthController : ControllerBase
+    {
+        public AuthController()
+        {
+
+        }
+
+        [Route("complete/{id}")]
+        [HttpPut]
+        public async Task<IActionResult> CompleteSprint(int id)
+        {
+            await _service.CompleteSprint(id);
+
+            return Ok();
+        }
+    }
+}
