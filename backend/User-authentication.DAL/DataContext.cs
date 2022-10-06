ec6e47a499b5d07824410ed3a18d03d6fe01f584
Vadis00 <v.gomenyuk@std.npu.edu.ua>
2022-10-06 14:09:45 +0300

    created database

---
+++ backend/User-authentication.DAL/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using User_authentication.Common.Entities;
+
+namespace User_authentication.DAL
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+
+    }
+}
--- backend/User-authentication.WebAPI/Program.cs
+++ backend/User-authentication.WebAPI/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using User_authentication.DAL;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DataContext>(
+    o => o.UseNpgsql(builder.Configuration["ConnectionStrings:UserAuthenticationDB"],
+        b => b.MigrationsAssembly(typeof(DataContext).Assembly.FullName))
+        .EnableDetailedErrors());
 
 var app = builder.Build();
 
