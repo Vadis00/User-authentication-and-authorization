a38b2b324448beae27c7a8b22bcba791008cc337
Vadis00 <v.gomenyuk@std.npu.edu.ua>
2022-10-17 19:09:46 +0300

    Fixed input

--- frontend/src/app/components/auth/auth.component.ts
+++ frontend/src/app/components/auth/auth.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.css']
+  styleUrls: ['./auth.component.css'],
 })
 export class AuthComponent implements OnInit {
+  public taskCreateForm: FormGroup = new FormGroup({});
+  public summaryControl: FormControl;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  public inputSearch = '';
+  constructor() {
+    this.summaryControl = new FormControl(this.inputSearch, [
+      Validators.required,
+      Validators.minLength(2),
+      Validators.maxLength(80),
+    ]);
   }
 
+  public test(): void {
+ 
+    console.log(this.inputSearch);
+  }
+  ngOnInit(): void {}
 }
